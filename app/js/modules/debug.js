9c1df2abcc2f6475fafb133a6be8d832265d26b4
Bogdan Blinnikov <bonkalol@list.ru>
2015-10-14 13:42:15 +0300

    init template

--- app/js/modules/debug.js
+++
@@ -1,9 +0,0 @@
-;(function () {
-
-	// remove trash from localStorage
-	localStorage.removeItem('font_css_cache');
-	localStorage.removeItem('font_css_cache_file');
-	localStorage.removeItem('inlineSVGdata');
-	localStorage.removeItem('inlineSVGrev');
-
-})();
